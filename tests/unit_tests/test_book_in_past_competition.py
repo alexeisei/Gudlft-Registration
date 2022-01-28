a875309921e1988563f95039fef2f31457eaa802
alexeisei <80957733+alexeisei@users.noreply.github.com>
2022-01-28 13:39:04 +0100

    Update test_book_in_past_competition.py

--- tests/unit_tests/test_book_in_past_competition.py
+++ tests/unit_tests/test_book_in_past_competition.py
@@ -8,15 +8,43 @@ def client():
     server.app.config['TESTING'] = True
     clients = server.app.test_client()
     return clients
-  
-  
-  def test_new_competitions_dates(client):
-      date = datetime.today()
-      date_competition = datetime.strptime(server.competitions[0]['date'], '%Y-%m-%d %H:%M:%S')
-      print(date)
-      print(date_competition)
-      if date_competition > date :
-         print(date)
-         print(date_competition)
-         assert date_competition == True
-          
+
+
+def test_new_competitions_dates(client):
+    """
+    check if the competition date is still valid
+    """
+    club = server.clubs[0]['name']
+    competition = server.competitions[0]
+    
+    result = client.post('/purchasePlaces', 
+                         data={'club': club,
+                                'competition': competition['name'],
+                                'numberOfPlaces': competition['numberOfPlaces'],
+                                'places': 5,
+                               })
+    assert result.status_code == 200
+
+     
+def test_past_competitions_dates(client):
+    """
+    check if the date of the competition is no longer valid
+    """
+    club = server.clubs[0]['name']
+    competition = server.competitions[0]
+    result = client.post('/purchasePlaces', 
+                         data={'club': club,
+                                'competition': competition['name'],
+                                'numberOfPlaces': competition['numberOfPlaces'],
+                                'places': 5,
+                               })
+    assert result.status_code == 200
+    
+    
+def test_booking_competition_good(client):
+    
+    club= server.clubs[0]['name']
+    competition = server.competitions[0]['name']
+    result = client.get(f'/book/{competition}/{club}')
+    
+    assert result.status_code == 200
