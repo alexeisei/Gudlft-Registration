1da77d72dfcf339d455386a70f21bab23e001ab5
alexeisei <80957733+alexeisei@users.noreply.github.com>
2022-01-28 14:17:56 +0100

    add test

--- tests/unit_tests/test_more_than_allowed_points.py
+++ tests/unit_tests/test_more_than_allowed_points.py
@@ -20,3 +20,15 @@ def test_use_more_than_their_point(client):
                                })
     assert result.status_code == 200
     
+    
+def test_use_less_than_their_point(client):
+    club = server.clubs[0]['name']
+    competition = server.competitions[0]
+    result = client.post('/purchasePlaces', 
+                         data={'club': club,
+                                'competition': competition['name'],
+                                'numberOfPlaces': competition['numberOfPlaces'],
+                                'places': 2,
+                               })
+    assert result.status_code == 200
+    
