3e8d9f3d47f0e1a427bdc152b6dff6a07d6577b5
alexeisei <80957733+alexeisei@users.noreply.github.com>
2022-01-28 14:06:20 +0100

    Update test_more_than_12_places.py

--- tests/unit_tests/test_more_than_12_places.py
+++ tests/unit_tests/test_more_than_12_places.py
@@ -30,3 +30,15 @@ def test_12_places(client):
                                 'places': 12,
                                })
     assert result.status_code == 200
+
+    
+    def test_less_than_12_places(client):
+    club = server.clubs[0]['name']
+    competition = server.competitions[0]
+    result = client.post('/purchasePlaces', 
+                         data={'club': club,
+                                'competition': competition['name'],
+                                'numberOfPlaces': competition['numberOfPlaces'],
+                                'places': 3,
+                               })
+    assert result.status_code == 200
