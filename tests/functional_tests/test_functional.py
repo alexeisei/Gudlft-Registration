eb91d2ae5310c91f7ecfdbeb8612104e7eba2818
alexeisei <80957733+alexeisei@users.noreply.github.com>
2022-02-01 15:45:50 +0100

    Added functionnal test

--- tests/functional_tests/test_functional.py
+++ tests/functional_tests/test_functional.py
@@ -10,3 +10,33 @@ def client():
 
 
 def test_user(client):
+    response = client.get('/')
+
+    club = server.clubs[2]
+    competition = server.competitions[0]
+
+    assert response.status_code == 200
+    response = client.post('/showSummary',
+                          data={'email': club['email']})
+    assert b'Welcome, kate@shelifts.co.uk' in response.data
+    assert response.status_code == 200
+
+    points = int(club['points'])
+    response = client.post('/purchasePlaces',
+                          data={'club': club['name'],
+                                'competition': competition['name'],
+                                'places': 1}
+                          )
+    updated_points = int(club['points'])
+    assert response.status_code == 200
+    assert updated_points != points
+
+    points = int(club['points'])
+    response = client.post('/purchasePlaces',
+                          data={'club': club['name'],
+                                'competition': competition['name'],
+                                'places': 13}
+                          )
+    updated_points = int(club['points'])
+    assert response.status_code == 200
+    assert updated_points == points
