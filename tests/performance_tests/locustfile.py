f9ae66b310b5687bbdb5ba11baf05700be190080
alexeisei <80957733+alexeisei@users.noreply.github.com>
2022-02-01 14:18:08 +0100

    add locust performance test

--- tests/performance_tests/locustfile.py
+++ tests/performance_tests/locustfile.py
@@ -8,26 +8,31 @@ class HelloWorldUser(HttpUser):
     club = loadClubs()[0]["name"]
     email = loadClubs()[0]["email"] 
     
-    
     @task
     def index(self):
+        self.client.get("/")
     
     
     @task
-    def connexion(self):
-        
-   
+    def connexion(self):        
+        self.client.post("/showSummary", data={"email": self.email}, name="showSummary")
+    
     
     @task
-    def purchase_places(self):
-        
+    def purchase_places(self):       
+        self.client.post("/purchasePlaces", data={
+            "club": self.club,
+            "competition": self.competitions,
+            "places": 2,
+            }, name="purchasePlaces")
 
 
     @task
-    def get_place(self):
+    def get_place(self):       
+        self.client.get("/book/" + self.competitions 
+        + "/" + self.club, name="book")
         
-
     
     @task
     def logout(self):
-
+        self.client.get("/logout")
