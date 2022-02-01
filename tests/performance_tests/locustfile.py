cb733464d68c78c60f216230c188767e1771dfc3
alexeisei <80957733+alexeisei@users.noreply.github.com>
2022-02-01 14:10:21 +0100

    Update locustfile.py

--- tests/performance_tests/locustfile.py
+++ tests/performance_tests/locustfile.py
@@ -1,1 +1,33 @@
+from locust import HttpUser, task, between
+from server import loadClubs, loadCompetitions
+
+class HelloWorldUser(HttpUser):
+    
+    wait_time = between(1, 2)
+    competitions = loadCompetitions()[0]['name']
+    club = loadClubs()[0]["name"]
+    email = loadClubs()[0]["email"] 
+    
+    
+    @task
+    def index(self):
+    
+    
+    @task
+    def connexion(self):
+        
+   
+    
+    @task
+    def purchase_places(self):
+        
+
+
+    @task
+    def get_place(self):
+        
+
+    
+    @task
+    def logout(self):
 
